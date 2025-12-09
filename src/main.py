296f8868b0fa975d446879621f8533eb2aa0a029
zt-a <zt20061113@gmail.com>
2025-12-10 00:32:59 +0600

    fix api uri

--- src/main.py
+++ src/main.py
@@ -18,10 +18,10 @@ async def lifespan(app: FastAPI):
     
 app = FastAPI(lifespan=lifespan)
 
-app.include_router(auth_router)
+app.include_router(prefix=settings.api_v1_prefix, router=auth_router)
 app.include_router(prefix=settings.api_v1_prefix, router=essay_router)
 app.include_router(prefix=settings.api_v1_prefix, router=refprint_router)
-app.include_router(profile_router)
+app.include_router(prefix=settings.api_v1_prefix, router=profile_router)
 
 
 @app.get("/")
